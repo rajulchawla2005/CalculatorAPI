73b854ed9d6a2719cc281c91f76cbe8b13e26401
rajulchawla2005 <rajulchawla2005@gmail.com>
2025-02-25 16:01:50 +0000

    created redirects, they are incredibly bad rn but idk how else to implement them
    add button has a page now, put two inputs in but they aren't formatted great

---
+++ static/scripts/script.js
@@ -0,0 +1,7 @@
+function add_button_redirect() {
+	document.location.href="/static/subpages/add_button.html";
+}
+
+function home_button_redirect() {
+	window.location.href = "http://192.168.0.62:5000";
+}
