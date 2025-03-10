fe02964f3a39d1e70bf13bcb1bde33c83d80eb63
rajulchawla2005 <rajulchawla2005@gmail.com>
2025-03-10 15:42:53 +0000

    i think this is all the changes, finally moved all the pages into the dynamic one instead of the static one trying to avoid the reliance on fixed paths cuz shit happens good progress with direction using flask

--- api.py
+++ api.py
@@ -1,11 +1,26 @@
 import calculator
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for, redirect
 
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
+def home():
 	return render_template("index.html")
 	
+@app.route("/buttons")
+def add_button():
+	return render_template("buttons/add_button.html")
+def sub_button():
+	return render_template("buttons/sub_button.html")
+def mul_button():
+	return render_template("buttons/mul_button.html")
+def div_button():
+	return render_template("buttons/div_button.html")
+
+	
 if __name__ == "__main__":
-	app.run(host='0.0.0.0', port=5000, debug=True, use_reloader=True)
+	with app.test_request_context():
+		home_url = url_for('home')
+		
+		print("home_urL:", home_url)
+		app.run(host='0.0.0.0', port=5000, debug=True, use_reloader=True)
