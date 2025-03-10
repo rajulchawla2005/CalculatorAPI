93769b4030b815e8702271c33f76621a0a681b86
rajulchawla2005 <rajulchawla2005@gmail.com>
2025-03-10 17:57:08 +0000

    got the api calls working on the add button so should be easier now
    also got to make it all look nice now

--- api.py
+++ api.py
@@ -1,5 +1,6 @@
 import calculator
-from flask import Flask, render_template, url_for, redirect
+from flask import Flask, render_template, url_for, redirect, request, jsonify, after_this_request
+import json
 
 app = Flask(__name__)
 
@@ -16,6 +17,30 @@ def mul_button():
 	return render_template("buttons/mul_button.html")
 def div_button():
 	return render_template("buttons/div_button.html")
+	
+@app.route('/api_call_get', methods=['GET', 'POST'])
+def get_test():
+	if request.method == 'GET':
+		jsonResp = {"bro what are you tryna get??":1}
+		print(jsonResp)
+		return jsonify(jsonResp)
+	else:
+		print("bad request, get test")
+		return "Too sigma", 400
+
+@app.route('/api_call_add_post', methods=['GET', 'POST'])
+def add_post_call():
+	if request.method == 'POST':
+		inputs = request.data.decode("UTF-8")
+		json_dict = json.loads(inputs)
+		num1, num2 = json_dict['num1'], json_dict['num2']
+		answer = calculator.add(num1, num2)
+		jsonResp = {"output":answer}
+		print(jsonResp)
+		return jsonify(jsonResp)
+	else:
+		print("bad request, add post call")
+		return "Too sigma", 400	
 
 	
 if __name__ == "__main__":
--- static/scripts/script.js
+++ static/scripts/script.js
@@ -1,7 +1,59 @@
-function add_button_redirect() {
-	document.location.href="/static/subpages/add_button.html";
+function validate_input(num1, num2) {
+	num1 = parseInt(num1);
+	num2 = parseInt(num2);
+	
+	if ((Number.isFinite(num1)) && (Number.isFinite(num2))) {
+		return true;
+	}
+	else {
+		return false;
+	}
 }
 
-function home_button_redirect() {
-	window.location.href = "http://192.168.0.62:5000";
+
+// validate input from html using the function, then make an API call if input is all good
+function add_submit() {
+	// get the input from the html page
+	var num1 = document.getElementById('num1').value;
+	var num2 = document.getElementById('num2').value;
+	
+	
+	// if the input is valid
+	if (validate_input(num1, num2)) {
+		// parse the numbers
+		num1 = parseInt(num1);
+		num2 = parseInt(num2);
+		
+		// make an API call
+		const url = api_url;
+		
+		fetch(url, 
+		{
+			method: "POST",
+			body: JSON.stringify
+			({num1: num1, num2: num2}),
+			headers: {
+				"Content-type": "application/json",
+			},
+		})
+		
+		.then(response => response.json())
+		
+		.then(json => {
+			console.log(JSON.stringify(json));
+			const obj = JSON.parse(JSON.stringify(json));
+			console.log(obj.output);
+			alert(obj.output);
+		})
+		
+	}
+	// input is not valid
+	else {
+		alert("input is not valid");
+	}
+	
+	
+	
+
+	
 }
